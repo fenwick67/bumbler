72221390d6e7b4c08eb1727df22ff977e69c6076
fenwick67 <smileman66@gmail.com>
2017-05-14 18:20:39 -0500

    commit before metalsmithing

--- dev-serve/admin/app.js
+++ dev-serve/admin/app.js
@@ -643,7 +643,7 @@ function formFactory(keysLabels){
     var el = document.createElement('div');
     el.setAttribute('class','field')
     el.innerHTML = `
-    <label class="label">${label}
+    <label class="label">${label||key}
       <input class="input" type="text" name="${key}">
     </label>`
     ret.appendChild(el);
@@ -653,25 +653,39 @@ function formFactory(keysLabels){
 }
 
 
+
 module.exports = class SettingsManager extends Object{
   constructor(el){
     super();
-    var kl = {
+
+    this.labels = {
       publishUrl: "Git Publish URL",
       publishBranch: "Git Publish Branch",
       title:"Title",
       postsPerPage:"Posts per Page"
-    }
-    var form = formFactory(kl);
+    };
+
+    this.defaultKeys = Object.keys(this.labels);
+    this.defaults = _.mapValues(this.labels,l=>'');
+
+    var form = this.autoFormFactory(this.defaults);
+    this.form = form;
+    this.formContain = document.createElement('div');
     var btn = document.createElement('button');
+    var revertBtn = document.createElement('button');
     var btnContain = document.createElement('div')
     btnContain.setAttribute('class','field')
     btn.setAttribute('class','button is-primary');
     btn.innerHTML = "💾&#xFE0E; Save";
+    revertBtn.setAttribute('class','button is-warning');
+    revertBtn.innerHTML = '⮌&#xFE0E; Revert Changes';
     btnContain.appendChild(btn);
-    form.appendChild(btnContain);
+    btnContain.appendChild(revertBtn);
 
-    el.appendChild(form);
+
+    this.formContain.appendChild(form);
+    el.appendChild(this.formContain);
+    el.appendChild(btnContain);
 
     var self = this;
     var _save = (e)=>{
@@ -679,17 +693,94 @@ module.exports = class SettingsManager extends Object{
       self.save()
       return false;
     }
+    var _load = e=>{
+      e.preventDefault();
+      self.load(true);
+      return false;
+    }
     btn.addEventListener('click',_save)
+    revertBtn.addEventListener('click',_load);
 
     self.load();
 
   }
   save(){
-    console.log('TODO save');
+    var dat = {};
+    _.each(this.form.querySelectorAll('input'),function(input){
+      dat[input.getAttribute('name')] = input.value;
+    })
+    var ok = false;
+
+    fetch('/admin/settings',{
+      method:"POST",
+      body:JSON.stringify(dat),
+      headers:{'Content-Type': 'application/json'}
+    }).then(res=>{
+      ok = res.ok;
+      if (ok){
+        return res.json();
+      }else{
+        throw new Error('bad response')
+      }
+    }).then(json=>{
+      var formData = _.clone(json);
+      var parent = this.form.parentElement;
+      parent.removeChild(this.form);
+
+      this.form = this.autoFormFactory(formData);
+      parent.appendChild(this.form);
+      popup("Settings saved.",'success',)
+
+    }).catch(e=>{
+      popup(e,'danger','Error saving settings')
+    })
+
+
   }
-  load(){
-    console.log('todo: load')
+  load(alert){
+    var ok = false;
+    fetch('/admin/settings').then(res=>{
+      ok = res.ok;
+      return res.json();
+    }).then(json=>{
+      var formData = _.clone(json);
+      var parent = this.form.parentElement;
+      parent.removeChild(this.form);
+
+      this.form = this.autoFormFactory(formData);
+      parent.appendChild(this.form);
+      if(alert){
+        window.popup('Reverted settings to last save.','warning')
+      }
+
+    }).catch(e=>{
+      popup(e,'danger','Error fetching settings')
+    })
+
   }
+
+  autoFormFactory(keysValues){
+    var ret = document.createElement('div');
+
+    var extendedData = _.extend({},this.defaults,keysValues)
+    console.log(extendedData)
+    var orderedKeys = _.sortBy(_.keys(extendedData));
+
+    _.each(orderedKeys,(key)=>{
+      var value = extendedData[key]
+      console.log(key);
+      var el = document.createElement('div');
+      el.setAttribute('class','field')
+      el.innerHTML = `
+      <label class="label">${this.labels[key]||key}
+        <input class="input" type="text" name="${key}" value="${ value || this.defaults[key] || "" }">
+      </label>`
+      ret.appendChild(el);
+    });
+
+    return ret;
+  }
+
 }
 
 },{"lodash":13}],10:[function(require,module,exports){
