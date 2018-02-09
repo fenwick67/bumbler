8b098f9e55f157339d4f7a2adb4bf6b960bb8460
Drew Harwell <fenwick67@users.noreply.github.com>
2018-02-09 12:48:29 -0600

    Update cli.js

--- cli.js
+++ cli.js
@@ -11,6 +11,19 @@ var build = require('./lib/build')
 
 var help = `BUMBLER: the easy self-hosted microblog.
 
+This program is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------
 USAGE:
   bumbler help   => show this
   bumbler init   => initialize your microblog
